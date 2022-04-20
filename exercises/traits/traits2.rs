c3fd6ff3ae11fb2a3d0a4a9f96f429cc35678db0
SylvainDe <sylvain.desodt+github@gmail.com>
2022-04-20 18:31:14 +0200

    Solve traits

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -10,13 +10,19 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 //TODO: Add your code here
+impl AppendBar for Vec<String> {
+    //Add your code here
+    fn append_bar(self) -> Self {
+        let mut cloned_self = self.clone();
+        cloned_self.push(String::from("Bar"));
+        cloned_self
+    }
+}
 
 #[cfg(test)]
 mod tests {
