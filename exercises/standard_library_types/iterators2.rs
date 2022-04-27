62617b10bbbefc032b2093bac4b8bd7fb26386ad
SylvainDe <sylvain.desodt+github@gmail.com>
2022-04-27 20:40:44 +0200

    Solve standard library types - WIP

--- exercises/standard_library_types/iterators2.rs
+++ exercises/standard_library_types/iterators2.rs
@@ -3,8 +3,6 @@
 // can offer. Follow the steps to complete the exercise.
 // As always, there are hints if you execute `rustlings hint iterators2`!
 
-// I AM NOT DONE
-
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
@@ -12,7 +10,10 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => first.to_uppercase().to_string() + c.as_str(),
+        // Note other solutions from https://stackoverflow.com/questions/38406793/why-is-capitalizing-the-first-letter-of-a-string-so-convoluted-in-rust:
+        // Some(f) => f.to_uppercase().chain(c).collect(),
+        // Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
     }
 }
 
@@ -21,7 +22,8 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    //vec![]; <-- was this supposed to be used ?
+    words.iter().map(|s| capitalize_first(s)).collect()
 }
 
 // Step 3.
@@ -29,7 +31,9 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    capitalize_words_vector(words).join("")
+    // or
+    //words.iter().map(|s| capitalize_first(s)).collect()
 }
 
 #[cfg(test)]
